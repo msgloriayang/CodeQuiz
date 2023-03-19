dd2ec083bdc2092c388333b27d8bb0f203858a1f
msgloriayang <yanggloria91@gmail.com>
2023-03-19 01:19:38 -0500

    decided to  remove high scores link due to it not being required or listed in the acceptance criteria for the assignment

--- script2.js
+++
@@ -1,7 +0,0 @@
-// retrieve high scores from console log
-let highScores = localStorage.getItem('highScores');
-console.log(highScores);
-
-// display high scores on HTML page
-let highScoresDiv = document.getElementById('highScores');
-highScoresDiv.innerHTML = highScores;
